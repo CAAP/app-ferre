3ae0b3b79f80fc71e9bc30c1d9b4c855ba0b6065
Carlos Aguilar <capagp@gmail.com>
2016-01-13 23:59:17 +0100

    Update creation of DB file

--- ferre.lua
+++ ferre.lua
@@ -4,11 +4,11 @@ local fd = require'carlos.fold'
 local sql = require'carlos.sqlite'
 local st = require'carlos.string'
 
-local function encode( s ) return s:gsub('%.*',' '):gsub('Ñ','&Ntilde;'):gsub('"', '&quot;')
+local function encode( s ) return s:gsub('%.+',' '):gsub('Ñ','&Ntilde;'):gsub('"', '&quot;')
 end
 
 local function readIn( a )
-    local desc, clave, precio, fecha = encode( a[3] ), a[10], a[20], a[#a-1]
+    local desc, clave, precio, fecha = encode( a[3] ), a[10], a[20], a[23]
     return { clave, desc, precio, fecha }
 end
 
@@ -18,6 +18,6 @@ local datos = fs.dlmread('ferre.txt', '\t')
 
 conn.exec'CREATE TABLE IF NOT EXISTS datos (clave PRIMARY KEY, desc, precio NUMBER, fecha)'
 
-fd.slice( datos, fd.map( readIn ), st.status(#datos), sql.into'datos', conn )
+fd.slice( 100, datos, fd.map( readIn ), st.status(#datos), sql.into'datos', conn )
 
 
