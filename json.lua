5c57693cbe77e413e6abbf66aed683a86327eb93
Carlos Aguilar <capagp@gmail.com>
2016-01-13 21:18:27 +0100

    Add json support

---
+++ json.lua
@@ -0,0 +1,17 @@
+local fd = require'carlos.fold'
+local sql = require'carlos.sqlite'
+local fs = require'carlos.files'
+local st = require'carlos.string'
+
+local conn = sql.connect'ferre.db'
+
+local JSON = '{"clave":"$clave", "desc":"$desc", "precio":$precio, "fecha":"$fecha"}'
+
+local function tostr( a ) return JSON:gsub( '%$(%w+)', a ) end
+
+local N = conn.count'datos'
+
+
+local items = fd.reduce( conn.query'SELECT * FROM datos', fd.map( tostr ), st.status(N), fd.into, {} )
+
+fs.dump('ferret.json', '[' .. table.concat(items, ',') .. ']')
