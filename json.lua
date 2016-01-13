316b8118313822b8e82d2d9e3d8cf88df762e73f
Carlos Aguilar <capagp@gmail.com>
2016-01-14 00:02:32 +0100

    JSON uupdated

--- json.lua
+++ json.lua
@@ -5,7 +5,7 @@ local st = require'carlos.string'
 
 local conn = sql.connect'ferre.db'
 
-local JSON = '{"clave":"$clave", "desc":"$desc", "precio":$precio, "fecha":"$fecha"}'
+local JSON = '{clave:"$clave", desc:"$desc", precio:$precio, fecha:"$fecha"}'
 
 local function tostr( a ) return JSON:gsub( '%$(%w+)', a ) end
 
@@ -14,4 +14,4 @@ local N = conn.count'datos'
 
 local items = fd.reduce( conn.query'SELECT * FROM datos', fd.map( tostr ), st.status(N), fd.into, {} )
 
-fs.dump('ferret.json', '[' .. table.concat(items, ',') .. ']')
+fs.dump('ferre.json', '[' .. table.concat(items, ',') .. ']')
