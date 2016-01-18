dac70209f2d2b0a034867c53d5791302888eb1eb
Carlos Aguilar <capagp@gmail.com>
2016-01-18 21:39:34 +0100

    Changing JSON objects

--- json.lua
+++ json.lua
@@ -5,19 +5,21 @@ local st = require'carlos.string'
 
 local conn = sql.connect'ferre.db'
 
-local JSON = {'clave', 'desc', 'fecha', 'precio1', 'precio2', 'precio3', 'u1', 'u2', 'u3'}
+local JSON = {'clave', 'desc', 'fecha', 'precio1', 'u1', 'precio2', 'u2', 'precio3', 'u3'}
 
-local function asstr(x) return tonumber(x) or '"'..x..'"' end
+local function quot(x) return tonumber(x) or '"'..x..'"' end
 
-local function tostr(a)
-    local ret = fd.reduce( JSON, fd.filter(function(k) return a[k] end), fd.map(function(k) return k..':'..asstr(a[k]) end), fd.into, {} )
-    return '{' .. table.concat(ret, ', ') .. '}'
+local function tovec(a)
+    local ret = fd.reduce( JSON, fd.map(function(k) return quot(a[k] or '') end), fd.into, {} )
+    return '[' .. table.concat(ret, ', ') .. ']'
 end
 
 local N = conn.count'datosALL'
 
 local QRY = "SELECT *, ROUND(costol*impuesto*descuento*p1,2) precio1, ROUND(costol*impuesto*descuento*p2,2) precio2, ROUND(costol*impuesto*descuento*p3,2) precio3 FROM datosALL"
 
-local items = fd.reduce( conn.query(QRY), fd.map( tostr ), st.status(N), fd.into, {} )
+local items = fd.reduce( conn.query(QRY), fd.map( tovec ), st.status(N), fd.into, {} )
 
-fs.dump('ferre.json', '[' .. table.concat(items, ', ') .. ']')
+local keys = '[' .. table.concat(fd.reduce(JSON, fd.map(quot), fd.into, {}), ', ') .. ']'
+
+fs.dump('ferre.json', '{datos: ' .. table.concat(items, ', ') .. ', ks: ' .. keys .. '}')
