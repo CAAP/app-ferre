d117b178b1e0fa03b4ae29a1d4b3dc10da9cb37c
Carlos Aguilar <capagp@gmail.com>
2016-01-18 05:00:44 +0100

    Database modified.

--- ferre2.lua
+++ ferre2.lua
@@ -8,15 +8,15 @@ local function encode( s ) return s:gsub('%.+',' '):gsub('Ñ','&Ntilde;'):gsub('"
 end
 
 local function readIn( a )
-    local desc, costol, costo, clave, u1, p1, u2, p2, u3, p3, fecha = encode( a[3] ), a[5], a[8], a[10], a[11], a[13], a[14], a[16], a[17], a[19], a[23]
-    return { clave, desc, costol, costo, u1, p1, u2, p2, u3, p3, fecha }
+    local desc, costol, impuesto, descuento, clave, u1, p1, u2, p2, u3, p3, fecha = encode( a[3] ), a[5], 1.0+a[6]/100.0, 1.0-a[7]/100.0, a[10], a[11], a[13], a[14], a[16], a[17], a[19], a[23]
+    return { clave, desc, costol, impuesto, descuento, u1, p1, u2, p2, u3, p3, fecha }
 end
 
 local conn = sql.connect'ferre.db'
 
 local datos = fs.dlmread('ferre.txt', '\t')
 
-conn.exec'CREATE TABLE IF NOT EXISTS datosALL (clave PRIMARY KEY, desc, costol NUMBER, costo NUMBER, u1, p1 NUMBER, u2, p2 NUMBER, u3, p3 NUMBER, fecha)'
+conn.exec'CREATE TABLE IF NOT EXISTS datosALL (clave PRIMARY KEY, desc, costol NUMBER, impuesto NUMBER, descuento NUMBER, u1, p1 NUMBER, u2, p2 NUMBER, u3, p3 NUMBER, fecha)'
 
 fd.slice( 100, datos, fd.map( readIn ), st.status(#datos), sql.into'datosALL', conn )
 
