4c2d37154db70a92864df6969a3b7c9fa6188934
Carlos Aguilar <capagp@gmail.com>
2016-01-18 20:07:27 +0100

    FIX round PRC

--- json.lua
+++ json.lua
@@ -5,13 +5,13 @@ local st = require'carlos.string'
 
 local conn = sql.connect'ferre.db'
 
-local JSON = '{clave:"$clave", desc:"$desc", precio1:$precio1, precio2:$precio2, precio3:$precio3, unidad1:$u1, unidad2:$u2, unidad3:$u3, fecha:"$fecha"}'
+local JSON = '{clave:"$clave", desc:"$desc", precio1:$precio1, precio2:$precio2, precio3:$precio3, unidad1:"$u1", unidad2:"$u2", unidad3:"$u3", fecha:"$fecha"}'
 
-local function tostr( a ) return JSON:gsub( '%$(%w+)', a ) end
+local function tostr( a ) a.u2 = a.u2 or ''; a.u3 = a.u3 or ''; return JSON:gsub( '%$(%w+)', a ) end
 
 local N = conn.count'datosALL'
 
-local QRY = "SELECT *, costol*impuesto*descuento*p1 precio1, costol*impuesto*descuento*p2 precio2, costol*impuesto*descuento*p3 precio3 FROM datosALL"
+local QRY = "SELECT *, ROUND(costol*impuesto*descuento*p1,2) precio1, ROUND(costol*impuesto*descuento*p2,2) precio2, ROUND(costol*impuesto*descuento*p3,2) precio3 FROM datosALL"
 
 local items = fd.reduce( conn.query(QRY), fd.map( tostr ), st.status(N), fd.into, {} )
 
