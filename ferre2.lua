e662abcba33bb39d5455163f95b0e4564267fd95
Carlos Aguilar <capagp@gmail.com>
2016-01-20 07:46:32 +0100

    Solved issue w quotes

--- ferre2.lua
+++ ferre2.lua
@@ -8,8 +8,10 @@ local st = require'carlos.string'
 
 local function encode( s ) return s:gsub('%s+$',''):gsub('%.+$',''):gsub('"', '&quot;') end
 
+local function quot(s) return tonumber(s) and s or s:gsub('"', '&quot;') end
+
 local function readIn( a )
-    local desc, costol, impuesto, descuento, clave, u1, p1, u2, p2, u3, p3, fecha = encode( a[3] ), a[5], 1.0+a[6]/100.0, 1.0-a[7]/100.0, a[10], a[11], a[13], a[14], a[16], a[17], a[19], a[23]
+    local desc, costol, impuesto, descuento, clave, u1, p1, u2, p2, u3, p3, fecha = encode( a[3] ), a[5], 1.0+a[6]/100.0, 1.0-a[7]/100.0, a[10], quot(a[11]), a[13], quot(a[14]), a[16], quot(a[17]), a[19], a[23]
     return { clave, desc, costol, impuesto, descuento, u1, p1, u2, p2, u3, p3, fecha }
 end
 
