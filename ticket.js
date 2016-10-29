0948b853b4a09da08ed63706e91116e3fded0e00
Charlie Root <root@ferreb.rga.ip>
2016-10-29 08:41:01 -0500

    select instead of focus

--- ticket.js
+++ ticket.js
@@ -95,7 +95,7 @@
 		row.title = q.desc.substr(0,3); // TRYING OUT LOCATION XXX
 		row.dataset.clave = q.clave;
 		row.insertCell().appendChild( document.createTextNode( q.clave ) );
-		row.insertCell().appendChild( inputE( [['type', 'number'], ['size', 2], ['min', 0], ['name', 'qty'], ['value', q.qty]] ) ).focus();
+		row.insertCell().appendChild( inputE( [['type', 'number'], ['size', 2], ['min', 0], ['name', 'qty'], ['value', q.qty]] ) ).select(); // XXX focus()
 		let desc = row.insertCell();
 		if (q.faltante) { desc.classList.add('faltante'); }
 		desc.classList.add('basura'); desc.appendChild( document.createTextNode( q.desc ) );
