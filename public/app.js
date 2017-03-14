121e888537caf2b725dc359442d0cdc63bbb629b
Charlie Root <root@ferreBSD.bancomer.com>
2017-03-13 19:07:01 -0600

    OK New Order

---
+++ public/app.js
@@ -0,0 +1,183 @@
+        "use strict";
+
+	var ferre = {};
+
+	window.onload = function() {
+
+	    const PRICE = DATA.STORES.PRICE;
+
+	    DATA.inplace = q => {let r = document.body.querySelector('tr[data-clave="'+q.clave+'"]'); if (r) {DATA.clearTable(r); BROWSE.rows(q,r);} return q;};
+
+	    // BROWSE
+
+	    BROWSE.tab = document.getElementById('resultados');
+	    BROWSE.lis = document.getElementById('tabla-resultados');
+
+	    BROWSE.DBget = s => IDB.readDB( PRICE ).get( s );
+
+	    BROWSE.DBindex = (a, b, f) => IDB.readDB( PRICE ).index( a, b, f );
+
+	    ferre.keyPressed = BROWSE.keyPressed;
+
+	    ferre.startSearch = BROWSE.startSearch;
+
+	    ferre.scroll = BROWSE.scroll;
+
+	    ferre.cerrar = e => e.target.closest('dialog').close();
+
+	    // TICKET
+
+	    TICKET.bag = document.getElementById( TICKET.bagID );
+	    TICKET.myticket = document.getElementById( TICKET.myticketID );
+
+	    TICKET.show = () => { TICKET.myticket.style.display = 'block'; TICKET.myticket.style.visibility = 'visible'; }
+
+	    ferre.rabatt = function() {
+		function prom(row) {
+		    if (!row.classList.contains('rabatt')) {
+			let i = row.querySelector('input[name=rea]');
+			i.value = 7;
+			TICKET.update({target: i});
+		    }
+		}
+		Array.from(TICKET.bag.children).forEach(prom);
+	    };
+
+	    (function() {
+		const diagI = document.getElementById('dialogo-item');
+		const tcount = document.getElementById(TICKET.tcountID);
+		const ttotal = document.getElementById( TICKET.ttotalID );
+		const persona = document.getElementById('personas');
+		const diagF = document.getElementById('dialogo-faltante');
+		const obs = diagF.querySelector('input[name=obs]');
+
+		let clave = -1;
+		persona.dataset.id = 0;
+//		const tag = document.getElementById('tag');
+
+		function asnum(s) { let n = Number(s); return Number.isNaN(n) ? s : n; };
+		function uptoCents(q) { return Math.round( 100 * q[q.precio] * q.qty * (1-q.rea/100) ); };
+
+		TICKET.total = cents => { ttotal.textContent = ' $' + (cents / 100).toFixed(2); tcount.textContent = TICKET.items.size;};
+
+		TICKET.extraEmpty = () => { ttotal.textContent = ''; tcount.textContent = ''; };
+
+		ferre.emptyBag = () => {TICKET.empty(); return SQL.print({id_tag: 'd', pid: Number(persona.value)})};
+
+		ferre.menu = e => {
+		    if (persona.value == 0) { return; }
+		    clave = asnum(e.target.parentElement.dataset.clave); // XXX one can use this instead: diagI.returnValue = clave;
+		    diagI.showModal();
+		};
+
+		ferre.add2bag = function() {
+		    diagI.close();
+		    if (TICKET.items.has( clave )) { console.log('Item is already in the bag.'); return false; }
+		    return IDB.readDB( PRICE ).get( clave )
+			.then( w => { w.qty=1; w.precio='precio1'; w.rea=0; w.totalCents=uptoCents(w); return w; } )
+			.then( TICKET.add );
+		};
+
+	    ferre.print = function(a) {
+		if (TICKET.items.size == 0) {return Promise.resolve();}
+		const id_tag = TICKET.TAGS[a] || TICKET.TAGS.none;
+		const pid = Number(persona.dataset.id);
+
+		let objs = ['id_tag='+id_tag, 'pid='+pid]; // , 'rfc='+rfc // 'person='+(PEOPLE.id[pid] || 'NAP'), // 'tag='+a, // , 'count='+TICKET.items.size
+
+		TICKET.myticket.style.visibility = 'hidden';
+
+		TICKET.items.forEach( item => objs.push( 'args=' + TICKET.plain(item) ) );
+
+		return SQL.print( objs ).then( TICKET.empty, () => {TICKET.myticket.style.visibility = 'visible'} );
+	    };
+
+		ferre.enviarF = e => SQL.update({clave: clave, faltante: 1, obs: obs.value, tbname: 'faltantes', id_tag: 'u'}).then( () => { obs.value = ''; ferre.cerrar(e); } );
+
+		ferre.faltante = e => IDB.readDB( PRICE ).get( clave ).then( w => { ferre.cerrar(e); if (w.faltante < 2) {obs.value = w.obs; diagF.showModal();} } );
+
+	    // PEOPLE - Multi-User support
+
+		function data( o ) { return IDB.readDB( PRICE ).get( asnum(o.clave) ).then( w => Object.assign( o, w ) ).then( TICKET.add ); }
+		function a2obj( a ) { const M = a.length/2; let o = {}; for (let i=0; i<M; i++) { o[a[i*2]] = a[i*2+1]; } return o; }
+//		function recreate(q) { return  q.split('&').reduce( (p, s) => p.then( () => data(a2obj(s.split('+'))) ), Promise.resolve() ); }
+		let recreate = q => Promise.all( q.split('&').map(s => data(a2obj(s.split('+')))) ).then( () => Promise.resolve() ).then( () => {tcount.textContent = TICKET.items.size;} );
+		function tabs(k) { persona.dataset.id = k; if (PEOPLE.tabs.has(k)) { recreate(PEOPLE.tabs.get(k).query); } }
+
+		ferre.tab = () => {
+		    const pid = Number(persona.value);
+	/* message tag
+		    ferre.tag(); */
+		    ferre.print('guardar').then( () => tabs(pid) );
+//		    if (TICKET.items.size > 0) { ferre.print('guardar').then( () => tabs(pid) ); }
+//		    else { tabs(pid); }
+		};
+
+		ferre.saveme = () => { persona.value = 0; ferre.tab(); }
+
+		(function appendNobody(){
+		    let opt = document.createElement('option');
+		    opt.value = 0;
+		    opt.label = '';
+		    opt.selected = true;
+		    persona.appendChild(opt);
+		})();
+
+		PEOPLE.load().then( a => a.forEach( p => { let opt = document.createElement('option'); opt.value = p.id; opt.appendChild(document.createTextNode(p.nombre)); persona.appendChild(opt); } ) );
+
+
+	    })();
+
+	    ferre.updateItem = TICKET.update;
+
+	    ferre.clickItem = e => TICKET.remove( e.target.parentElement );
+
+/*
+	    ferre.surtir = function() {
+		let objs = [];
+		TICKET.items.forEach( item => objs.push( 'args=clave+'+item.clave+'+qty+'+item.qty  ) );
+		return XHR.get('/ticket/surtir.lua?'+objs.join('&')).then( ferre.saveme );
+	    };
+*/
+
+	    // SQL
+
+	    SQL.DB = document.location.origin + ':8081';
+
+	    // HEADER
+
+	    (function() {
+	        const note = document.getElementById('notifications');
+		let FORMAT = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+		function now(fmt) { return new Date().toLocaleDateString('es-MX', fmt) }
+		note.appendChild( document.createTextNode( now(FORMAT) ) );
+	    })();
+
+	    // SET FOOTER
+
+	    (function() { document.getElementById('copyright').innerHTML = 'versi&oacute;n ' + 2.0 + ' | cArLoS&trade; &copy;&reg;' })();
+
+	    // SERVER-SIDE EVENT SOURCE
+
+	    (function() {
+		function addEvents() {
+		    let esource = new EventSource(document.location.origin + ":8080");
+		    DATA.onLoaded(esource);
+		    esource.addEventListener("tabs", function(e) {
+		 	console.log("tabs event received.");
+		    	JSON.parse( e.data ).forEach( o => PEOPLE.tabs.set(o.pid, o) );
+		    }, false);
+		    esource.addEventListener("delete", function(e) {
+			const pid = Number(e.data);
+			PEOPLE.tabs.delete(pid);
+			console.log('Remove ticket for: ' + PEOPLE.id[pid]);
+		    }, false);
+		}
+
+	    // LOAD DBs
+ 		if (IDB.indexedDB)
+		    IDB.loadDB( DATA ).then( () => console.log('Success!') ).then( addEvents );
+	    })();
+
+	};
+
---
+++ public/ticket.js
@@ -0,0 +1,161 @@
+	"use strict";
+
+	var TICKET = { bagID: 'ticket-compra', myticketID: 'ticket', ttotalID: 'ticket-total', tivaID: 'ticket-iva', tbrutoID: 'ticket-bruto', tcountID: 'ticket-count' };
+
+	(function() {
+	    const VARS = ['clave', 'qty', 'rea', 'precio', 'totalCents'];
+	    const EVARS = ['clave', 'qty', 'desc', 'rea', 'prc', 'subTotal' ];
+	    const TAGS = {none: 'x', presupuesto: 'a', ticket: 'b', facturar: 'c', guardar: 'g', impreso: 'I', pagado: 'P', facturado: 'F'};
+	    TAGS.ID = {x: 'none', a: 'presupuesto', b: 'ticket', c: 'facturar', g: 'guardar'};
+
+	    TICKET.TAGS = TAGS;
+
+	    TICKET.items = new Map();
+
+	    function tocents(x) { return (x / 100).toFixed(2); };
+
+	    function uptoCents(q) { return Math.round( 100 * q[q.precio] * q.qty * (1-q.rea/100) ); };
+
+	    function asnum(s) { let n = Number(s); return Number.isNaN(n) ? s : n; };
+
+	    function clearTable(tb) { while (tb.firstChild) { tb.removeChild( tb.firstChild ); } }; //recycle?
+
+	    function incdec(e) {
+		switch (e.key || e.which) {
+		    case '+':
+		    case 'Add':
+		    case 187: case 107:
+			e.target.value++;
+			e.preventDefault();
+			TICKET.update(e); //ferre.updateItem(e);
+			break;
+		    case '-':
+		    case 'Subtract':
+		    case 189: case 109:
+			if (e.target.value == 1) { e.preventDefault(); break; }
+			e.target.value--;
+			e.preventDefault();
+			TICKET.update(e); //ferre.updateItem(e);
+			break;
+		    default: break;
+		}
+	    }
+
+	    function inputE( a ) {
+		let ret = document.createElement('input');
+		ret.addEventListener('keydown', incdec);
+		a.map( o => ret[o[0]] = o[1] );
+		return ret;
+	    }
+
+	    function bagTotal() {
+		let total = 0;
+		TICKET.items.forEach( item => { total += asnum(item.totalCents); } );
+		TICKET.total( total );
+	    }
+
+	    function precios(q) {
+		if ((q.precio2 == 0) && (q.precio3 == 0)) { return document.createTextNode( q.precios.precio1 ); }
+		let ret = document.createElement('select');
+		ret.name = 'precio';
+		for (let k in q.precios) {
+		    let opt = document.createElement('option');
+		    opt.value = k; opt.selected = (q.precio == k);
+		    opt.appendChild( document.createTextNode( q.precios[k] ) );
+		    ret.appendChild(opt);
+		}
+		return ret;
+	    }
+
+	    function israbatt(q, row, prev) {
+		let rabatt = asnum(q.rea) > 0 || q.precio != 'precio1';
+		if (rabatt ^ prev) {
+		    if (rabatt)
+			row.classList.add('rabatt');
+		    else
+			row.classList.remove('rabatt');
+		}
+	    }
+
+	    function displayItem(q) {
+		let row = TICKET.bag.insertRow(0);
+		row.title = q.desc.substr(0,3); // TRYING OUT LOCATION XXX
+		row.dataset.clave = q.clave;
+		israbatt(q, row, false);
+		// DATOS
+		row.insertCell().appendChild( document.createTextNode( q.clave ) );
+		row.insertCell().appendChild( inputE( [['type', 'number'], ['size', 2], ['min', 0], ['name', 'qty'], ['value', q.qty]] ) ).select();
+		let desc = row.insertCell();
+		if (q.faltante) { desc.classList.add('faltante'); }
+		desc.classList.add('basura'); desc.appendChild( document.createTextNode( q.desc ) );
+		let pcs = row.insertCell();
+		pcs.classList.add('pesos'); pcs.appendChild( precios(q) );
+		let rea = inputE( [['type', 'number'], ['size', 2], ['name', 'rea'], ['value', q.rea]] );
+		let td = row.insertCell(); td.appendChild(rea); td.appendChild( document.createTextNode('%'));
+		let total = row.insertCell();
+		total.classList.add('pesos'); total.classList.add('total'); total.appendChild( document.createTextNode( tocents(q.totalCents) ) );
+	    }
+
+	    function showItem(q) {
+		let row = TICKET.bag.insertRow();
+		row.dataset.clave = q.clave;
+		israbatt(q, row, false);
+		// DATOS
+		q.subTotal = tocents(q.totalCents);
+		q.prc = q.precios[q.precio];
+		EVARS.forEach( v => row.insertCell().appendChild( document.createTextNode( q[v] ) ) );
+		row.lastChild.classList.add('total');
+	    }
+
+	    TICKET.plain = o => VARS.map( v => { return (v + '+' + o[v] || '') } ).join('+');
+
+	    TICKET.update = function(e) {
+		let tr = e.target.parentElement.parentElement;
+		let lbl = tr.querySelector('.total');
+		let clave = asnum( tr.dataset.clave );
+		let k = e.target.name;
+		let v = asnum( e.target.value );
+		let items = TICKET.items;
+
+		e.target.value = v;
+
+		if (k == 'qty' && v == 0) { return TICKET.remove(tr); }
+
+		if (items.has( clave )) {
+		    let q = items.get( clave );
+		    let rabatt = (q.rea > 0 || q.precio != 'precio1');
+		    q[k] = v;
+		    israbatt(q, tr, rabatt);
+		    q.totalCents = uptoCents(q); // partial total
+		    items.set( clave, q );
+		    lbl.textContent = tocents(q.totalCents);
+		    bagTotal();
+		}
+	    };
+
+	    TICKET.add = function(w) {
+		TICKET.myticket.style.visibility = 'visible';
+		TICKET.items.set( w.clave, w );
+		displayItem( w );
+		bagTotal();
+	    };
+
+	    TICKET.show = function(w) {
+		TICKET.myticket.style.visibility = 'visible';
+		TICKET.items.set( w.clave, w );
+		showItem( w );
+		bagTotal();
+	    };
+
+	    TICKET.remove = function(tr) {
+		let clave = asnum( tr.dataset.clave );
+		TICKET.items.delete( clave );
+		TICKET.bag.removeChild( tr );
+		if (!TICKET.bag.hasChildNodes()) { TICKET.empty(); } else { bagTotal(); } // TICKET.myticket.style.visibility = 'hidden';
+	    };
+
+	    TICKET.empty = function() { TICKET.items.clear(); clearTable( TICKET.bag ); TICKET.myticket.style.visibility = 'hidden'; TICKET.extraEmpty(); };
+
+	    })();
+
+
