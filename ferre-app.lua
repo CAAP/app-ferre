99aafb6d6e3b6cc8edc517ffbf8f544fa47d26dc
alberto <alberto@routeBJ.gateway.huawei.net>
2019-01-24 17:53:33 -0600

    sdfkjksdf

--- ferre-app.lua
+++ ferre-app.lua
@@ -1,11 +1,13 @@
---module setup
+#!/usr/bin/env lua53
 
 -- Import section
-local zmq = require'lzmq'
+local zmq = require'carlos.zmq'
 local fd = require'carlos.fold'
 local html = require'carlos.html'
 
 local concat = table.concat
+local print = print
+local assert = assert
 
 -- No more external access after this point
 _ENV = nil
@@ -16,14 +18,18 @@ local HELLO = html.response{content='stream'}
 local PEERS = {}
 
 -- Local function for module-only access
-local function handshake(srv)
+local function handshake(srv, k)
     local id, more = srv.receive()
     if more then
-	local PEERS[id] = true
+	PEERS[id] = true
 	print(id, '\n', concat(more, '\n'))
-	return true
+	return id
     else
-	return handshake(srv)
+	if k == 5 then
+	    return false
+	else
+	    return handshake(srv, k+1)
+	end
     end
 end
 
@@ -31,10 +37,14 @@ end
 --  SCRIPT running, execution  --
 ---------------------------------
 
-local server = zmq.stream'tcp://localhost:8080'
+local server = zmq.stream'tcp://192.168.3.1:8080'
 
 while true do
-    handshake(server)
+    local id = handshake(server, 0)
+    if id then
+	PEERS[id] = server.send(id, HELLO)
+    else
+	print'ERROR: peer could not be reached!'
+    end
 end
-
 
