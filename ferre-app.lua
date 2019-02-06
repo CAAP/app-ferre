921ea84ddd940f1ee83f205ad1a00f1f54a7dfc7
SmartGoggles <sg@medaid02.medaid.app>
2019-02-06 13:44:47 -0600

    sdlkfj4

--- ferre-app.lua
+++ ferre-app.lua
@@ -1,45 +1,123 @@
-#!/usr/bin/env lua53
 
--- Import section
-local zmq = require'carlos.zmq'
-local fd = require'carlos.fold'
-local html = require'carlos.html'
+-- Import Section
+local bsd  = require'carlos.bsd'
+local zmq  = require'carlos.zmq'
+local fd   = require'carlos.fold'
+local sql  = require'carlos.sqlite'
+local json = require'carlos.json'
 
-local concat = table.concat
-local print = print
+local iopen = require'carlos.io'.open
+local dump = require'carlos.files'.dump
+local time = os.time
+local date = os.date
+
+local format = require'string'.format
 local assert = assert
 
+local print = print
+
 -- No more external access after this point
-_ENV = nil
+_ENV = nil -- or M
 
--- Local variables for module-only access (private)
-local HELLO = html.response{content='stream'}
+-- Local Variables for module-only access
+--
+local VERS = {} -- week, vers
+local TABS = {tickets = 'uid, id_tag, clave, desc, costol NUMBER, unidad, precio NUMBER, qty INTEGER, rea INTEGER, totalCents INTEGER',
+	      updates = 'vers INTEGER PRIMARY KEY, clave, campo, valor'}
 
-local PEERS = {}
+--[[
+    local keys = { uid=1, id_tag=2, clave=3, precio=4, qty=5, rea=6, totalCents=7 }
+    local clause = string.format("WHERE uid LIKE '%s%%'", today)
+    local query = "SELECT uid, SUM(qty) count, SUM(totalCents) totalCents, id_tag FROM %q WHERE uid LIKE '%s%%' GROUP BY uid||id_tag" -- id_tag CHANGES
+    local QRY = string.format('SELECT uid, SUM(qty) count, SUM(totalCents) totalCents, id_tag FROM %q %s GROUP BY uid', tbname, clause)
+--]]
 
--- Local function for module-only access
-local function handshake(srv, k)
-    local id, more = srv.receive()
-    if #more == 0 then
-	PEERS[id] = true
-	id, more = srv.receive()
-    end
-    print(id, '\t', #more, '\n', concat(more, '\n'), '\n')
-    return id
-end
-
----------------------------------
---  SCRIPT running, execution  --
----------------------------------
-
-local server = zmq.stream'tcp://192.168.3.1:8080'
-
-while true do
-    local id = handshake(server, 0)
-    if id then
-	PEERS[id] = server.send(id, HELLO)
+--------------------------------
+-- Local function definitions --
+--------------------------------
+-- accepts name of db and adds absolute path,
+-- creating it if required, otherwise it checks
+-- whether path already exists, always returning
+-- the sql connection or error
+local function dbconn(path, create)
+    local f = format('/home/sg/db/%s.db', path) -- XXX alberto
+    if create or bsd.file_exists(f) then
+	return sql.connect(f)
     else
-	print'ERROR: peer could not be reached!'
+	return false, format('ERROR: File %q does not exists!', f)
     end
 end
+
+-- if db file exists and 'updates' tb exists then returns count
+local function which( db )
+    local conn = dbconn( db )
+    if conn and conn.exists'updates' then
+	return conn.count'updates'
+    else return 0 end
+end
+
+local function now() return time()-21600 end
+
+local function asweek(t) return date('Y%YW%U', t) end
+
+local function version(w)
+    local hoy = now()
+    local week = asweek( hoy )
+    local vers = which( week )
+    local semana = 3600 * 24 * 7
+    while vers == 0 do -- change in YEAR XXX
+	hoy = hoy - semana
+	week = asweek( hoy )
+	vers = which( week )
+--	if week:match'W00' then break end
+    end
+    w.week = week; w.vers = vers
+    return w
+end
+
+local function updateVERS(w) dump("/var/www/htdocs/app-ferre/ventas/json/version.json", json.asJSON(w)) end
+
+local function newTicket( w )
+    local uid = date('%FT%TP', now()) .. w.pid
+    fd.reduce( w.args, fd.map( hd.args(keys, uid, w.id_tag) ), sql.into( tbname ), conn ) -- ids( uid, w.id_tag ), 
+
+end
+
+local function asJSON() return format('data: %s', json.asJSON()) end -- XXX
+
+local function sse( w ) -- XXX
+    if not(w) then return ':empty' end
+    local event = w.event
+    w.event = nil
+    local ret = w.ret or {}
+    w.ret = nil
+    local data = w.data or asJSON( w )
+    ret[#ret+1] = 'event: ' .. event
+    ret[#ret+1] = 'data: ['
+    ret[#ret+1] = data
+    ret[#ret+1] = 'data: ]'
+    ret[#ret+1] = '\n'
+    return table.concat( ret, '\n')
+end
+
+-- Let's build a REP socket using inproc protocol XXX
+
+
+---------------------------------
+-- Public function definitions --
+---------------------------------
+
+local conn = assert( dbconn(asweek(now()), true) )
+
+local QUERY  = format(sql.newTable, TBNAME, SCHEMA)
+
+assert( conn.exec(QUERY) )
+
+version(VERS) -- latest version for UPDATES
+
+print('\n\tWeek:', VERS.week, '\n\tVers:', VERS.vers)
+
+updateVERS(VERS) -- write to hard-disk latest version for UPDATES
+
+
 
