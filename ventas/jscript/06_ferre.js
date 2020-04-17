4ab9e249983c53a0f3d474228d3383c49df8cc2f
SmartGoggles <sg@medaid02.medaid.app>
2020-04-17 04:48:56 -0500

    sssss

--- ventas/jscript/06_ferre.js
+++ ventas/jscript/06_ferre.js
@@ -4,27 +4,6 @@
 //	    clickItem: e => TICKET.remove( e.target.parentElement ),
 	    xget: (q,o) => XHR.get( ferre.origin + q + '?' + UTILS.asstr(o) ),
 	    xpost: (q, o) => XHR.post( ferre.origin + q, UTILS.asstr(o) )
-
-
-	    guardar: function temporal(s) {
-		const PRICE = DATA.STORES.PRICE;
-		let ret = [];
-		IDB.readDB( PRICE ).openCursor(cursor => {
-		if (cursor) {
-		    let a = cursor.value;
-		    ret.push( a );
-		    cursor.continue();
-		} else {
-		    let b = new Blob([JSON.stringify(ret)], {type: 'application/json'});
-		    let a = document.createElement('a');
-		    let url = URL.createObjectURL(b);
-		    a.href = url;
-		    a.download = 'datos.json'
-		    a.click();
-		    URL.revokeObjectURL(url);
-		} });
-	    }
-
 	};
 
 	(function() {
