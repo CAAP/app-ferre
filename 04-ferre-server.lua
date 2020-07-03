6cb8e01a4245f36584a2f7038edea365a5236d89
carlos <carlos@hashy.Home>
2020-07-02 22:00:15 -0500

    222

--- 04-ferre-server.lua
+++ 04-ferre-server.lua
@@ -7,11 +7,12 @@ local fd	  = require'carlos.fold'
 local sse	  = require'carlos.html'.response
 local ssevent	  = require'carlos.ferre'.ssevent
 local receive	  = require'carlos.ferre'.receive
+local send	  = require'carlos.ferre'.send
 local monitor	  = require'carlos.zmq'.monitor
 
+local pollin	  = require'lzmq'.pollin
 local context	  = require'lzmq'.context
-local sbind	  = require'socket'.bind
-local sselect	  = require'socket'.select
+local pid	  = require'lzmq'.pid
 local hex	  = require'lints'.hex
 
 local format	  = require'string'.format
@@ -21,39 +22,28 @@ local remove	  = table.remove
 local assert	  = assert
 local print	  = print
 local pairs	  = pairs
-local ipairs	  = ipairs
 local toint	  = math.tointeger
-local time	  = os.time
 
 -- No more external access after this point
 _ENV = nil -- or M
 
 -- Local Variables for module-only access
 --
-local ENDPOINT	 = 5030 -- 'tcp://*:5030'
+local ENDPOINT	 = 'tcp://*:5030'
 local UPSTREAM   = 'ipc://upstream.ipc'
 local SPIES	 = 'inproc://espias'
 local HELLO      = sse{content='stream'}
 
 local FRTS	   = {'apple', 'apricot', 'avocado', 'banana', 'berry', 'cherry', 'coconut', 'cucumber', 'fig', 'grape', 'raisin', 'guava', 'pepper', 'corn', 'plum', 'kiwi', 'lemon', 'lime', 'lychee', 'mango', 'melon', 'olive', 'orange', 'durian', 'longan', 'pea', 'peach', 'pear', 'prune', 'pine', 'pomelo', 'pome', 'quince', 'rhubarb', 'mamey', 'soursop', 'granate', 'sapote'}
 
-local FRUITS	 = {}
 
-local TT = time()
+local FRUITS	 = {}
+local SKS	 = {}
 
 --------------------------------
 -- Local function definitions --
 --------------------------------
 --
-
-local function send(fruit, msg)
-    local sk = FRUITS[fruit]
-    if sk and not(sk:send(msg)) then
-	sk:close()
-	FRUITS[fruit] = nil
-	insert(FRTS, fruit)
-    end
-end
 
 local function distill(msg)
     local ev, d = msg:match'(%a+)%s([^!]+)'
@@ -61,31 +51,28 @@ local function distill(msg)
     else return 'SSE',':empty' end
 end
 
-local function sk2fruit( sk )
-    local fruit = remove(FRTS)
-    FRUITS[fruit] = sk
+local function id2fruit( id, sk )
+    local fruit = remove(FRTS) -- pid()
+    FRUITS[fruit] = id
+    SKS[sk] = fruit
     return fruit
 end
 
-local function handshake(server)
-    local sk = assert( server:accept() )
-    assert( sk:settimeout(2) )
-    local msg, e = sk:receive()
-    if not e then
-	local fruit = sk2fruit(sk)
-	send(fruit, HELLO)
-	send(fruit, ssevent('fruit', fruit))
-	return 'New fruit: '..fruit
-    end
+local function handshake(server, sk)
+    local id = server:recv_msgs()[1] -- receive(server, true)
+    local fruit = id2fruit(id, sk)
+	send(server, id, HELLO)
+	send(server, id, ssevent('fruit', fruit))
+    return 'New fruit: '..fruit
 end
 
 local function broadcast(server, msg, fruit)
     if fruit then
-	send(fruit, msg)
+	send(server, FRUITS[fruit], msg)
 	return format('Broadcast %s to %s', msg, fruit)
     else
 	local j = 0
-	for ft in pairs(FRUITS) do send(ft, msg); j = j + 1 end
+	for _,id in pairs(FRUITS) do send(server, id, msg); j = j + 1 end
 	return format('Message %s broadcasted to %d peers', msg, j)
     end
 end
@@ -104,22 +91,48 @@ local function switch(msgs, server)
     end
 end
 
+local function sayonara(server, sk)
+    local fruit = SKS[sk]
+    if not fruit then return ':empty' end
+    send(server, FRUITS[fruit], '') -- close socket
+    FRUITS[fruit] = nil
+    SKS[sk] = nil
+    insert(FRTS, fruit)
+    return fruit
+end
+
+local function default(server)
+    local id = server:recv_msg(true) -- receive(server, true)
+    send(server, id, '') -- close socket
+    return 'not supported'
+end
+
 ---------------------------------
 -- Program execution statement --
 ---------------------------------
 --
 -- Initialize servers
-
-local server = assert( sbind('*', ENDPOINT) )
-
-assert( server:settimeout(0) )
-
-print('\nSuccessfully bound to port:', ENDPOINT, '\n')
--- -- -- -- -- --
---
-
 local CTX = context()
 
+local server = assert(CTX:socket'STREAM')
+-- -- -- -- -- --
+-- * MONITOR *
+local spy = monitor(CTX, server, SPIES)
+-- -- -- -- -- --
+-- ***********
+assert( server:notify(false) )
+
+assert( server:alive(true) )
+
+assert( server:linger(0) )
+
+assert( server:timeout(3) )
+
+assert( server:bind( ENDPOINT ) )
+
+print('\nSuccessfully bound to:', ENDPOINT, '\n')
+-- -- -- -- -- --
+--
 local msgs = assert(CTX:socket'PULL')
 
 assert( msgs:bind( UPSTREAM ) )
@@ -130,23 +143,31 @@ print('\nSuccessfully bound to:', UPSTREAM, '\n')
 --
 print( 'Starting servers ...', '\n' )
 
-local SKTS = {server, msgs}
-SKTS[server] =  function() return handshake(server) end
-SKTS[msgs] = function() return switch(msgs, server) end
-
 --
---
-
 while true do
+print'+\n'
 
-    print'+\n'
+    if pollin{msgs, spy} then
 
-    if (TT - time()) > 100 then TT = time(); broadcast(server, ':empty\n') end
+	if msgs:events() == 'POLLIN' then
+	    print( switch(msgs, server), '\n' )
+	end
 
-    local sks = sselect(SKTS, nil, -1)
+	if spy:events() == 'POLLIN' then
+	    local ev, sk, addr = spy:receive() -- receive(spy)
+	    print( ev, hex(sk), addr, '\n' )
+	    if addr:match'tcp' then
+		if ev:match'DISCONNECTED' then
+		    print( 'Bye bye', sayonara(server, sk), '\n')
+		elseif ev:match'ACCEPTED' then
+		    print( handshake(server, sk), '\n' )
+--		elseif ev:match'LISTENING' then
+--		    print('Event', ev, default(server), '\n')
+		end
+	    end
+	end
 
-    for _,s in ipairs(sks) do print(SKTS[s](), '\n') end
-
+    end
 end
 ---]]
 
