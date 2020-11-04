4d7b6f8be42f378c359de95f1c607f01d450813a
carlos <carlos@ferry.cdmx>
2020-11-03 19:30:08 -0600

    222

--- 08-ferre-peer.lua
+++ 08-ferre-peer.lua
@@ -2,13 +2,11 @@
 
 -- Import Section
 --
-local tabs	  = require'carlos.ferre.tabs'
-local asUUID	  = require'carlos.ferre.uuids'
 local reduce	  = require'carlos.fold'.reduce
 local receive	  = require'carlos.ferre'.receive
 local context	  = require'lzmq'.context
 local pollin	  = require'lzmq'.pollin
---local keypair	  = require'lzmq'.keypair
+local keypair	  = require'lzmq'.keypair
 
 local rconnect	  = require'redis'.connect
 local posix	  = require'posix.signal'
@@ -20,26 +18,17 @@ local insert	  = table.insert
 local remove	  = table.remove
 local format	  = string.format
 local print	  = print
-local type	  = type
 
 local STREAM	  = os.getenv'STREAM_IPC'
+local TIENDA	  = os.getenv'TIENDA'
 
---local VULTR	  = "tcp://192.168.1.110:5630" -- os.getenv'VULTR'
+local VULTR	  = "tcp://192.168.1.110:5630" -- os.getenv'VULTR'
 
 -- No more external access after this point
 _ENV = nil -- or M
 
 -- Local Variables for module-only access
 --
-local TABS	  = { tabs=true, delete=true,
-		      msgs=true, login=true }
-
-local INMEM	  = { query=true, rfc=true, bixolon=true,
-		      uid=true,     feed=true,
-		      ledger=true,  adjust=true }
-
-local FERRE 	  = { update=true, faltante=true, eliminar=true }
-
 local client	  = assert( rconnect('127.0.0.1', '6379') )
 
 local SRVK	  = "/*FTjQVb^Hgww&{X*)@m-&D}7Lxk?f5o7mIe=![2"
@@ -49,37 +38,6 @@ local SRVK	  = "/*FTjQVb^Hgww&{X*)@m-&D}7Lxk?f5o7mIe=![2"
 --------------------------------
 --
 
-local function sendAll(skt, tag, msg)
-    insert(msg, 1, tag)
-    print( 'Sent to', tag, skt:send_msgs(msg), '\n' )
-end
-
-local function broadcast(skt, msg)
-    local msg = msg or ': OK\n\n'
-    local N = 1
-    if type(msg) == 'table' then
-	reduce(msg, function(m) skt:send_msgs{'SSE', m} end)
-	N = #msg
-    else
-	skt:send_msgs{'SSE', msg}
-    end
-    print( 'Broadcasting', N, 'message(s)\n\n+\n' )
-end
-
-local function switch(msg)
-    local cmd = msg[2]
-    if cmd == 'ticket' then
-	local k = 'queue:tickets:'..msg[3]
-	local ret = client:lrange(k, 0, -1)
-	insert(ret, 1, 'ticketx') -- ADD uid |¬ msg[3]  XXX
-	return ret
-
-    elseif cmd == 'updatex' then
-	remove(msg, 1)
-	return msg
-
-    end
-end
 
 ---------------------------------
 -- Program execution statement --
@@ -97,7 +55,6 @@ posix.signal(posix.SIGINT, shutdown)
 --
 -- Initilize server(s)
 --
-
 local CTX = context()
 
 local stream = assert(CTX:socket'ROUTER')
@@ -107,6 +64,24 @@ assert( stream:mandatory(true) ) -- causes error in case of unroutable peer
 assert( stream:bind( STREAM ) )
 
 print('\nSuccessfully bound to:', STREAM, '\n')
+--
+-- -- -- -- -- --
+--
+local msgr = assert(CTX:socket'DEALER')
+
+assert( msgr:immediate(true) )
+
+assert( msgr:linger(0) )
+
+assert( msgr:set_id(TIENDA) )
+
+assert( keypair():client(msgr, SRVK) )
+
+assert( msgr:connect( VULTR ) )
+
+print('\nSuccessfully connected to', VULTR, '\n')
+
+msgr:send_msg'OK'
 
 --
 -- -- -- -- -- --
@@ -115,7 +90,7 @@ print('\nSuccessfully bound to:', STREAM, '\n')
 while true do
 print'+\n'
 
-    pollin{stream}
+    pollin{stream, msgr}
 
     if stream:events() == 'POLLIN' then
 
@@ -132,11 +107,9 @@ print'+\n'
 	elseif cmd == 'inmem' then
 	    stream:send_msgs( msg )
 
---[[
 	elseif cmd == 'vultr' then
 	    msgr:send_msgs( switch( msg ) )
 	    print'\nVULTR: Message sent for cloud storage\n'
---]]
 
 	----------------------
 	-- divide & conquer --
@@ -161,7 +134,6 @@ print'+\n'
 
 	end
 
---[[
     elseif msgr:events() == 'POLLIN' then
 
 	local id, msg = receive( stream )
@@ -176,7 +148,6 @@ print'+\n'
 	    stream:send_msgs( msg )
 
 	end
---]]
 
     end
 
