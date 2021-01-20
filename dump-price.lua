f660725dfcc653ab859c3c8312cee93c91eb033d
carlos <carlos@puff.petct.innn>
2021-01-20 15:03:28 -0600

    sd2

--- dump-price.lua
+++ dump-price.lua
@@ -1,16 +1,19 @@
 #! /usr/bin/env lua53
 
 local fd	= require'carlos.fold'
-
-local asJSON	= require'json'.encode
 local dbconn	= require'carlos.ferre'.dbconn
 local dump	= require'carlos.files'.dump
+
+local asJSON	= require'json'.encode
+local rconnect	= require'redis'.connect
+local insert	= table.insert
 
 local HOME	= require'carlos.ferre'.HOME
 
 _ENV =  nil
 
-local DEST = HOME .. '/json/precios.json' -- '/ventas/json/precios.json'
+local DEST 	= HOME .. '/json/precios.json' -- '/ventas/json/precios.json'
+local client	= assert( rconnect('127.0.0.1', '6379') )
 
 local function nulls(w)
     if w.precio2 == 0 then w.precio2 = nil end
@@ -21,7 +24,9 @@ end
 local conn = dbconn'ferre'
 local QRY  = 'SELECT * FROM precios WHERE desc NOT LIKE "VV%"'
 
-dump(DEST, asJSON(fd.reduce(conn.query(QRY), fd.map(nulls), fd.into, {})))
+local vers = client:get'app:updates:version'
+
+dump(DEST, asJSON(insert(fd.reduce(conn.query(QRY), fd.map(nulls), fd.into, {}), vers)))
 
 conn.close()
 
